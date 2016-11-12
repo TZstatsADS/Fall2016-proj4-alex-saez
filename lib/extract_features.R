5481f21ef9680bb9bf0bb1dc2363861edac88e28
Alex Saez <as3171@columbia.edu>
2016-11-12 14:33:00 -0500

    code for creating feature matrix

--- lib/extract_features.R
+++ lib/extract_features.R
@@ -2,14 +2,36 @@
 library(dplyr)
 
 extract_features = function(song){
-  # Extract relevant features from song
+  # Extract relevant features from song and return them as data frame
+  # INPUT: song in h5 format
+  # OUTPUT: single-row data frame of relevant song features
   
-  features1 = song$songs %>% select(danceability, energy, key, loudness, mode, tempo, time_signature)
+  gen_features = song$songs %>% select(danceability, energy, key, loudness, mode, tempo, time_signature)
   
-  pitches = rowSums(song$segments_pitches)/sum(song$segments_pitches)
+  pitches = rowMeans(song$segments_pitches)
   pitches = data.frame(pitch = matrix(pitches, nrow=1))
   
-  timbres = rowSums(song$segments_timbre)/sum(song$segments_timbre)
+  timbres = rowMeans(song$segments_timbre)
   timbres = data.frame(timbre = matrix(timbres, nrow=1))
   
+  loudness = rep(0, 12)
+  loudness_vals = table(findInterval(song$segments_loudness_max, seq(-20, 0, length.out=11)))
+  loudness[as.integer(names(loudness_vals))+1] = loudness_vals/sum(loudness_vals)
+  loudness = data.frame(loudness = matrix(loudness, nrow=1))
+  
+  return(cbind(gen_features, pitches, timbres, loudness))
 }
+
+
+# loop over all songs and create X matrix
+all_songs = list.files('../data/songs/')
+X = data.frame()
+for(i in 1:length(all_songs)){
+  print(i)
+  song = h5read(paste("../data/songs/", all_songs[i], sep=""), '/analysis')
+  X = rbind(X, extract_features(song))
+}
+
+save(X, file='../output/features.RData')
+
+
