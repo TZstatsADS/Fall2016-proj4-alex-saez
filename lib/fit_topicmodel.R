fb0f55bd8a7324d3162003df818dc1b0d4a1852a
Alex Saez <as3171@columbia.edu>
2016-11-12 19:17:57 -0500

    update

--- lib/fit_topicmodel.R
+++ lib/fit_topicmodel.R
@@ -4,22 +4,25 @@ load("../data/lyr.RData")
 song_ids = lyr$`dat2$track_id`
 lyr = lyr[, -c(1:3,6:30)]
 
-# convert lyr into a  document-term matrix in order to use 'topicmodels'
+# convert lyr into 2 document-term matrices (train and test) in order to use 'topicmodels'
 songs = apply(lyr, 1, function(x){paste(rep(names(x)[x>0], x[x>0]), collapse=' ')})
-corp = VCorpus(VectorSource(songs))
-dtm = DocumentTermMatrix(corp, control = list(wordLengths = c(0, Inf)))
+train_ind = sample(2350, 2000)
+corp_Tr = VCorpus(VectorSource(songs[train_ind]))
+corp_Te = VCorpus(VectorSource(songs[-train_ind]))
+dtm_Tr = DocumentTermMatrix(corp_Tr, control = list(wordLengths = c(0, Inf)))
+dtm_Te = DocumentTermMatrix(corp_Te, control = list(wordLengths = c(0, Inf)))
 
 # setdiff(names(lyr), dtm$dimnames$Terms)
 
 # fit LDA models with different numbers of topics
-topicmodel_10 = LDA(dtm, k=10, method="Gibbs")
-topicmodel_20 = LDA(dtm, k=20, method="Gibbs")
-topicmodel_30 = LDA(dtm, k=30, method="Gibbs")
-topicmodel_40 = LDA(dtm, k=40, method="Gibbs")
+topicmodel_10 = LDA(dtm_Tr, k=10, method="Gibbs")
+topicmodel_20 = LDA(dtm_Tr, k=20, method="Gibbs")
+topicmodel_30 = LDA(dtm_Tr, k=30, method="Gibbs")
+topicmodel_40 = LDA(dtm_Tr, k=40, method="Gibbs")
 
-# select best model using perplexity measure:
-p_10 = perplexity(topicmodel_10, newdata = dtm)
-p_20 = perplexity(topicmodel_20, newdata = dtm)
-p_30 = perplexity(topicmodel_30, newdata = dtm)
-p_40 = perplexity(topicmodel_40, newdata = dtm)
+# select best model using perplexity measure on testing set:
+p_10 = perplexity(topicmodel_10, newdata = dtm_Te)
+p_20 = perplexity(topicmodel_20, newdata = dtm_Te)
+p_30 = perplexity(topicmodel_30, newdata = dtm_Te)
+p_40 = perplexity(topicmodel_40, newdata = dtm_Te)
 plot(c(p_10, p_20, p_30, p_40))
