8948a3466090affddc073955d55280fd3d43d5da
alex-saez <as3171@columbia.edu>
2016-11-13 15:03:52 -0500

    topic models update

--- lib/fit_topicmodel.R
+++ lib/fit_topicmodel.R
@@ -19,10 +19,27 @@ topicmodel_10 = LDA(dtm_Tr, k=10, method="Gibbs")
 topicmodel_20 = LDA(dtm_Tr, k=20, method="Gibbs")
 topicmodel_30 = LDA(dtm_Tr, k=30, method="Gibbs")
 topicmodel_40 = LDA(dtm_Tr, k=40, method="Gibbs")
+topicmodel_50 = LDA(dtm_Tr, k=50, method="Gibbs")
+topicmodel_80 = LDA(dtm_Tr, k=80, method="Gibbs")
+topicmodel_100 = LDA(dtm_Tr, k=100, method="Gibbs")
+topicmodel_120 = LDA(dtm_Tr, k=120, method="Gibbs")
 
 # select best model using perplexity measure on testing set:
 p_10 = perplexity(topicmodel_10, newdata = dtm_Te)
 p_20 = perplexity(topicmodel_20, newdata = dtm_Te)
 p_30 = perplexity(topicmodel_30, newdata = dtm_Te)
 p_40 = perplexity(topicmodel_40, newdata = dtm_Te)
-plot(c(p_10, p_20, p_30, p_40))
+p_50 = perplexity(topicmodel_50, newdata = dtm_Te)
+p_80 = perplexity(topicmodel_80, newdata = dtm_Te)
+p_100 = perplexity(topicmodel_100, newdata = dtm_Te)
+p_120 = perplexity(topicmodel_120, newdata = dtm_Te)
+plot(c(p_10, p_20, p_30, p_40, p_50, p_80, p_100, p_120))
+
+topicmodels = list(tm10 = topicmodel_10, 
+                   tm20 = topicmodel_20, 
+                   tm30 = topicmodel_30, 
+                   tm40 = topicmodel_40, 
+                   tm50 = topicmodel_50)
+
+save(topicmodels, file='../output/topicmodels.RData')
+
