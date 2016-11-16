e4935378cb7630c94c316d5b742eb72c5c3f43b0
alex-saez <as3171@columbia.edu>
2016-11-16 18:39:03 -0500

    function to predict likely words given song features

--- lib/predict_words.R
+++ lib/predict_words.R
@@ -1,27 +1,36 @@
 
-load('../output/tree_classifier_10.RData')
-classifier = treemodel
-load('../output/topicmodel_10.RData')
-topicmodel=tm
-
-predict_words = function(song, topicmodel, classifier){
-   # INPUT: 
-   #   - song: analysis part of song in h5 format (e.g. song = h5read(filename, '/analysis'))
-   #   - topicmodel
-   #   - classifier
-   # OUTPUT: 
-   #   - p: word probabilities
+predict_words = function(song, topicmodel, classifier, wordlist){
+  # INPUT: 
+  #   - song: analysis part of song in h5 format (e.g. song = h5read(filename, '/analysis'))
+  #   - topicmodel
+  #   - classifier
+  #   - wordlist (optional): character vector of words to rank from most to least probable to occur
+  # OUTPUT: 
+  #   - word_ranks: if argument wordlist supplied, ranks words in wordlist from most to least probable; 
+  #   OR,
+  #   - p_words: if argument wordlist missing, word probabilities for the terms in topicmodel 
   
   library(rpart)
   library(topicmodels)
   source('../lib/extract_features.R')
   
-  if(classifier$parms$prior != topicmodel@k)
+  if(length(classifier$parms$prior) != topicmodel@k)
     stop('Number of topics in topicmodel must match number of classifier categories')
   
   X = extract_features(song)
   p_topics = predict(classifier, newdata = X)
   
+  p_words = p_topics %*% exp(topicmodel@beta)
+  names(p_words) = topicmodel@terms
   
+  if(!missing(wordlist)){
+    probs_for_ranking = -p_words[wordlist]
+    probs_for_ranking[is.na(probs_for_ranking)] = 0
+    names(probs_for_ranking) = wordlist
+    word_ranks = rank(probs_for_ranking, ties.method='average')
+    return(word_ranks)
+  }
+  else
+    return(p_words)
   
 }
